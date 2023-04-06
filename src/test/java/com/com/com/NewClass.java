e155aecbeeae463715d7cf574ac05778c04c13ab
MohammadTazrian <tazrianahmed1993@gmail.com>
2023-04-05 20:57:39 -0400

    added a new class

---
+++ src/test/java/com/com/com/NewClass.java
@@ -0,0 +1,21 @@
+package com.com.com;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		System.out.println("using this class to upload something on the github");
+		
+		NewClass obj = new NewClass();
+
+		obj.systemOut();
+		
+	}
+
+	public void systemOut() {
+		
+		System.out.println("just printing something from the method");
+	}
+	
+	
+	
+}
